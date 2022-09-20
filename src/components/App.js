c8e4dbde06f3a5c003fb42ab25f6b857d246484a
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-09-20 15:04:33 -0300

    initial layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
 export default function App(){
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     return(
-        <h1>Jogo da Forca</h1>
+        <div class="app">
+            <div class="game">
+                <img src="./src/assets/forca0.png"/>
+                <button>Escolher Palavra</button>
+            </div>
+            <div class="letters">
+                {alphabet.map(letter => <button>{letter.toUpperCase()}</button>)}
+            </div>
+            <div class="guess">
+                <p>Já sei a palavra!</p>
+                <input />
+                <button>Chutar</button>
+            </div>
+        </div>
     );
 }
