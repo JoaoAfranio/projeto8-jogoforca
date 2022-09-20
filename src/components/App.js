79b25e43a42bcedd8c164fdeab9a1b4232186c82
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-09-20 15:54:05 -0300

    sorting word

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
+import words from "./words";
+import React from "react";
+
 export default function App(){
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const [word, setWord] = React.useState("");
+    const [splitedWord, setSplitedWord] = React.useState("");
+    const [guessedLetters, setGuessedLetters] = React.useState([]);
+
+
+    function randomWord() {
+        return Math.floor(words.length * Math.random());
+    }
+
+    function selectWord(){
+        const word = words[randomWord()];
+        setWord(word);
+        setSplitedWord(word.split(""));
+    }
+
+   
     return(
         <div class="app">
             <div class="game">
                 <img src="./src/assets/forca0.png"/>
-                <button>Escolher Palavra</button>
+                <button onClick={selectWord}>Escolher Palavra</button>
+                <p>
+                    {splitedWord.length > 0 ? splitedWord.map(
+                        l => {
+                            return guessedLetters.indexOf(l) !== -1 ? <span>{l}</span> : <span>_</span>;
+                        }
+                        
+                    ) : ""}
+                </p>
             </div>
             <div class="letters">
                 {alphabet.map(letter => <button>{letter.toUpperCase()}</button>)}
